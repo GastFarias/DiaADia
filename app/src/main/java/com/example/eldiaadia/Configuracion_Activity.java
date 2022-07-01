a802e130e6eb8be6120211f87eb8c3e028bd966d
GastFarias <87099175+GastFarias@users.noreply.github.com>
2022-06-30 23:00:23 -0300

    Se agregaron metodos para manipular el Configuracion_Activity durante la seleccion de la hora de notificaciones (estetica )

--- app/src/main/java/com/example/eldiaadia/Configuracion_Activity.java
+++ app/src/main/java/com/example/eldiaadia/Configuracion_Activity.java
@@ -5,20 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
 public class Configuracion_Activity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private Button buttonCambiarHora;
+    private Button buttonGuardarHora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+
+        timePicker = findViewById(R.id.Configuracion_TimePicker);
+        buttonCambiarHora = findViewById(R.id.CambiarHora_button);
+        buttonGuardarHora = findViewById(R.id.guardarHora_button);
+
     }
+
     public void irFrase(View view){
         Intent i = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(i);
     }
+
     public void irAgregarAvisos(View view){
         Intent i = new Intent(getApplicationContext(),AgregarAvisos_Activity.class);
         startActivity(i);
     }
+
+    public void btn_CambiarHora(View view){
+        timePicker.setVisibility(View.VISIBLE);
+        buttonGuardarHora.setVisibility(View.VISIBLE);
+        buttonCambiarHora.setVisibility(View.GONE);
+
+    }
+
+    public void btn_GuardarHora(View view){
+        timePicker.setVisibility(View.GONE);
+        buttonCambiarHora.setVisibility(View.VISIBLE);
+        buttonGuardarHora.setVisibility(View.GONE);
+        // Toast con la hora seleccionada
+        // Tomar la hora y guardarla para las notificaciones
+
+    }
+
 }
